d374e0ec8c2529eba4fcd9455d73602c8c65b220
LiviuVisovan <127055057+LiviuVisovan@users.noreply.github.com>
2024-04-16 17:04:54 +0300

    added search card functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import { Card } from "./components/Card.js";
 import wordList from "./word-list.js";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="App">
       <div className="page-header">
@@ -15,9 +18,18 @@ function App() {
         trebui sa existe conform realității lingvistice. Un gol/lacună lexicală.
       </p>
       <h2>Lista nonlogismelor actuale</h2>
-      {wordList.map((word) => (
-        <Card key={word.slug} {...word} />
-      ))}
+      <input
+        placeholder="Cauta..."
+        type="text"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+      />
+      {query}
+      {wordList
+        .filter((word) => word.slug.includes(query))
+        .map((word) => (
+          <Card key={word.slug} {...word} />
+        ))}
     </div>
   );
 }
